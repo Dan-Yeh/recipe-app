f8bf838d1c943ff12f223a8ca3be32abde6758a9
Chen-Chen Yeh <ge96noj@mytum.de>
2021-05-06 14:32:30 +0200

    Add recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -62,3 +62,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Spaghetti',
+            time_minutes=15,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
